4180e59bbe4929e94698ca4cb7c039e033cc63ae
dat77 <42274048+dat77@users.noreply.github.com>
2019-10-17 11:21:26 +0300

    Add files via upload

---
+++ LoginController.java
@@ -0,0 +1,60 @@
+package ua.kiev.prog.oauth2.loginviagoogle.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    private static String authorizationRequestBaseUri = "oauth2/authorization";
+
+    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @GetMapping("/oauth_login")
+    public String getLoginPage(Model model) {
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+                .as(Iterable.class);
+        if (type != ResolvableType.NONE &&
+                ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+
+        clientRegistrations.forEach(registration ->
+                oauth2AuthenticationUrls.put(registration.getClientName(),
+                        authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+
+        return "oauth_login";
+
+
+//        ClientRegistration clientRegistration = clientRegistrationRepository
+//                .findByRegistrationId("google");
+//
+//        model.addAttribute("name_google", clientRegistration.getClientName());
+//        model.addAttribute("url_google", authorizationRequestBaseUri + "/" +
+//                clientRegistration.getRegistrationId());
+//
+//        clientRegistration = clientRegistrationRepository
+//                .findByRegistrationId("facebook");
+//
+//        model.addAttribute("name_facebook", clientRegistration.getClientName());
+//        model.addAttribute("url_facebook", authorizationRequestBaseUri + "/" +
+//                clientRegistration.getRegistrationId());
+//
+//        return "oauth_login";
+
+
+    }
+}
